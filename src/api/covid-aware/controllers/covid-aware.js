561cb93a5ec9d91bd870bb56909c840dac0d2954
Kane <phoboes@live.com>
2022-12-09 12:21:20 +1100

    Services removed, homepage rooms assosciation removed due to new strapi's deep nesting, format changes.

--- src/api/covid-aware/controllers/covid-aware.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * covid-aware controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::covid-aware.covid-aware');
--- src/api/covid-aware/services/covid-aware.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * covid-aware service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::covid-aware.covid-aware');
