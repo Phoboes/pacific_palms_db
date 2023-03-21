23116f9a04a1bd469a5daef5b04800d9c251ca7f
Kane <Phoboes@live.com>
2023-03-22 00:33:22 +1100

    Recreated gallery image categories, smoothing merge conflicts.

---
+++ src/api/gallery-image-category/controllers/gallery-image-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gallery-image-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::gallery-image-category.gallery-image-category');
