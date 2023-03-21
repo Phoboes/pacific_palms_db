fc2ec53ab71a714778081effb51c42de89cdab57
Kane <Phoboes@live.com>
2023-03-21 22:40:10 +1100

    Image gallery is back in.

---
+++ src/api/gallery-image-category/routes/gallery-image-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gallery-image-category router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::gallery-image-category.gallery-image-category');
---
+++ src/api/gallery-image-category/services/gallery-image-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gallery-image-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::gallery-image-category.gallery-image-category');
