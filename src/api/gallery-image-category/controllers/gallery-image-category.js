35c9dc730b5d4614531960f7349a5a238cb8c678
Kane <Phoboes@live.com>
2023-03-22 00:19:03 +1100

    Revert "Image gallery is back in."
    This reverts commit fc2ec53ab71a714778081effb51c42de89cdab57.

--- src/api/gallery-image-category/controllers/gallery-image-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * gallery-image-category controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::gallery-image-category.gallery-image-category');
--- src/api/gallery-image-category/services/gallery-image-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * gallery-image-category service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::gallery-image-category.gallery-image-category');
